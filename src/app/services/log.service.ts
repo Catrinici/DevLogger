0b3ca9014ede1941f1fbee31ec92cb237f72dc1e
Catrinici <catrinici.antonina@gmail.com>
2020-04-24 11:58:59 -0700

    final commit

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -14,20 +14,68 @@ export class LogService {
 	private logSource = new BehaviorSubject<Log>({ id: null, text: null, date: null })
 	selectedLog = this.logSource.asObservable();
 
+	private stateSource = new BehaviorSubject<boolean>(true);
+	stateClear = this.stateSource.asObservable();
+
 	constructor() {
-		this.logs = [
-			{ id: '1', text: 'Generated components', date: new Date('04/23/2020 5:09:23') },
-			{ id: '2', text: 'Added Bootstrap', date: new Date('04/22/2020 4:09:23') },
-			{ id: '3', text: 'Added logs components', date: new Date('04/21/2020 7:09:23') },
-		]
+		// 	this.logs = [
+		// 		{ id: '1', text: 'Generated components', date: new Date('04/23/2020 5:09:23') },
+		// 		{ id: '2', text: 'Added Bootstrap', date: new Date('04/22/2020 4:09:23') },
+		// 		{ id: '3', text: 'Added logs components', date: new Date('04/21/2020 7:09:23') },
+		// 	]
+		// }
+		this.logs = [];
 	}
 
 	getLogs(): Observable<Log[]> {
-		return of(this.logs);
+		if (localStorage.getItem('logs') === null) {
+			this.logs = [];
+		} else {
+			this.logs = JSON.parse(localStorage.getItem('logs'));
+		}
+
+		return of(this.logs.sort((a, b) => {
+			return b.date - a.date
+		}));
+
 	}
 
 	setFormLog(log: Log) {
 		this.logSource.next(log);
 
+	}
+
+	addLog(log: Log) {
+		this.logs.unshift(log)
+
+		//Add to localstorage
+		localStorage.setItem('logs', JSON.stringify(this.logs))
+	}
+
+	updateLog(log: Log) {
+		this.logs.forEach((cur, index) => {
+			if (log.id === cur.id) {
+				this.logs.splice(index, 1);
+			}
+		});
+		this.logs.unshift(log);
+
+		//Update log in local storage 
+		localStorage.setItem('logs', JSON.stringify(this.logs))
+	}
+
+	deleteLog(log: Log) {
+		this.logs.forEach((cur, index) => {
+			if (log.id === cur.id) {
+				this.logs.splice(index, 1);
+			}
+		});
+
+		// Delete log from LocalStorage
+		localStorage.setItem('logs', JSON.stringify(this.logs))
+	}
+
+	clearState() {
+		this.stateSource.next(true);
 	}
 }
